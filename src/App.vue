6ae42852fc277c40ee8790b9edfe26806acf667c
JJaeggm <raptor1130@gmail.com>
2024-01-10 12:19:32 +0900

    FIRST COMMIT

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1
+    :style="styleColor"
+  >{{ msg }}</h1>
+  <h2
+    style="color: royalblue; font-size: 40px;"
+  >{{ count }}</h2>
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
+  <button @click="reset">reset</button>
+  <div v-if="count > 5" class="box">
+    <p>directiv box</p>
+  </div>
+  <ul>
+    <Fruit 
+    v-for="fruit of fruits" 
+    :key="fruit" 
+    :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Fruit from './components/FruitItem.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components : {
+    Fruit : Fruit
+  },
+  data() {
+    return {
+      msg : "Hello World",
+      count : 0,
+      fruits : ["Apple", "Orange", "Banana", "Cherry"],
+      styleColor : {
+        backgroundColor : 'royalblue',
+      }
+    }
+  },
+  methods : {
+    increase() {
+      this.count += 1
+    },
+    decrease() {
+      this.count -= 1
+    },
+    reset() {
+      this.count = 0
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+body {
+  font-family: 'Pretendard-Regular';
+}
+h1 {
+  font-size: 50px;
+  color: orange;
+}
+button {
+  width: 150px; height: 30px; border: none;
+  border-bottom: 1px solid gray;
+  background-color: transparent;
+  cursor: pointer;
+  &:nth-of-type(2) {
+    margin: 0 10px;
+  }
+}
+.box {
+  border: 1px solid red;
+  margin-top: 20px;
+  p {
+    font-size: 32px;
+  }
 }
 </style>
---
+++ src/components/FruitItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <a href="javascript:void(0);">
+      {{ name }}
+    </a>
+  </li>
+</template>
+
+<script>
+  export default {
+    props : {
+      name : {
+        type : String,
+        default : ""
+      }
+    }
+  }
+</script>
